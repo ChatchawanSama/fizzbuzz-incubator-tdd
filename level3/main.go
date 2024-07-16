60f19747d9a55303e46a69728d483f879f5abc40
Chatchawan Sama <chatchawan.s@arise.tech>
2024-07-16 23:28:35 +0700

    [Level3][Round1] Pass test when input is 3

--- level3/main.go
+++ level3/main.go
@@ -2,6 +2,6 @@ package level3
 
 // FizzBuzz with no if and no switch statement
 func FizzBuzz(n int) string {
-	arr := []string{"1", "2"}
+	arr := []string{"1", "2", "Fizz"}
 	return arr[(n%10)-1]
 }
--- level3/main_test.go
+++ level3/main_test.go
@@ -18,4 +18,12 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("FizzBuzz(2) = %s, want %s", result, expected)
 		}
 	})
+
+	t.Run("should return Fizz when input is 3", func(t *testing.T) {
+		result := FizzBuzz(3)
+		expected := "Fizz"
+		if result != expected {
+			t.Errorf("FizzBuzz(3) = %s, want %s", result, expected)
+		}
+	})
 }
